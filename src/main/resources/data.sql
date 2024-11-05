a063abac658eec19b49b3d1ef5487091b7bc35eb
Augusto Diaz <adiazpol17@correo.unicordoba.edu.co>
2024-11-04 21:44:49 -0500

    mas empleados compilado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 insert into employees (id, name, email, password) values (1, 'Oscar Padilla', 'oscar@ogam.com', '12345678')
+insert into employees (id, name, email, password) values (2, 'Augusto Diaz', 'augusto@ogam.com', '12345678')
+insert into employees (id, name, email, password) values (3, 'Maryi Brieva', 'maryi@ogam.com', '12345678')
+insert into employees (id, name, email, password) values (4, 'Guillermo Castaño', 'guillermo@ogam.com', '12345678')
